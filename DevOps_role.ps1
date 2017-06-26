9fad595256a7a64c7ea540aabffa9902d11c7f4d
wayekoxodise <wayekoxodise@16-1-2-0-0-XFN-POC.VELA.COM>
2017-06-26 14:31:18 -0700

    Abstracts install methods into a shared module for simplicity.  Adds cygwin and pip install methods.  Adds aws pip package and some cygwin packages.

--- DevOps_role.ps1
+++ DevOps_role.ps1
@@ -5,6 +5,7 @@ Requirements:
 run "Set-ExecutionPolicy Bypass" first.
 3. cd into containing directory and run script with .\DevOps_role.ps1
 #>
+Import-Module $PSScriptRoot/modules/Vela-Utils.psm1 -Force
 
 # Searchable list of apps available by running 'choco search <packagename>'
 $apps = @(
@@ -35,6 +36,11 @@ $apps = @(
   "slack"
 )
 
+# Searchable list of Python packages available by running 'pip3 search <packagename>'
+$pip_packages = @(
+  "awscli"
+)
+
 # Searchable list of windows features available by running 'Get-WindowsFeature'
 $windows_features = @(
   "Telnet-Client"
@@ -43,8 +49,17 @@ $windows_features = @(
 $remote_files = @{
   "https://downloads.dcos.io/binaries/cli/windows/x86-64/dcos-1.9/dcos.exe" = "C:\ProgramData\chocolatey\bin\dcos.exe";
   # Wox is an Alfred equivalent launcher for Windows: Option + Spacebar
-  "https://github.com/Wox-launcher/Wox/releases/download/v1.3.424/Wox-1.3.424.exe" = "C:\ProgramData\chocolatey\bin\Wox.exe"
+  "https://github.com/Wox-launcher/Wox/releases/download/v1.3.424/Wox-1.3.424.exe" = "C:\ProgramData\chocolatey\bin\Wox.exe";
+  # apt-cyg is a package manager for cygwin.
+  "https://raw.githubusercontent.com/transcode-open/apt-cyg/master/apt-cyg" = "C:\tools\cygwin\bin\apt-cyg"
 }
+
+# Searchable list of Cygwin packages available at https://cygwin.com/cgi-bin2/package-grep.cgi
+$cygwin_packages = @(
+  "python",
+  "ruby",
+  "git"
+)
 
 $paths = @(
   "C:\Python36\Scripts\",
@@ -59,95 +74,19 @@ $paths = @(
   "C:\Program Files (x86)\Microsoft VS Code\bin"
 )
 
-[System.Collections.ArrayList]$installed_successfully = @( )
-[System.Collections.ArrayList]$install_failed = @( )
-
 function Configure-Vela-Workspace() {
-  Install-Chocolatey
-  Install-Apps
-  Install-WinFeatures
-  Download-Files
-  Add-Paths
+  Install-Apps -Apps $apps
+  Install-WinFeatures -Features $windows_features
+  Get-RemoteFiles -Remote_Files $remote_files
+  Add-Paths -Paths $paths
+  Install-PipPackages -Packages $pip_packages
+  Install-CygwinPackages -Packages $cygwin_packages
   Show-Report
   Execute-PostInstall
 }
 
 function Execute-PostInstall() {
   Write-Host "`nRun 'Wox.exe' once to start an Alfred-like launcher."
-}
-
-function Install-Chocolatey() {
-  if(-Not (Get-Command choco -errorAction SilentlyContinue)) {
-    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-  }
-}
-
-function Install-Apps() {
-  foreach ($app in $apps) {
-    choco install $app -y
-    Trap-Status -item $app
-  }
-}
-
-function Install-WinFeatures() {
-  foreach ($feature in $windows_features) {
-    Install-WindowsFeature $feature
-    Trap-Status -item $feature
-  }
-}
-
-function Download-Files() {
-  foreach ($remote_file in $remote_files.GetEnumerator()) {
-    if (-Not (Test-Path $remote_file.Value)) {
-      Download-File -Source $remote_file.Name.toString() -Destination $remote_file.Value
-    } else {
-      Write-Host $remote_file.Name.toString() " is already installed."
-    }
-    Trap-Status -item $remote_file.Name
-  }
-}
-
-function Download-File($Source, $Destination) {
-  Write-Host "Downloading $source to $destination"
-    try {
-      (New-Object System.Net.WebClient).DownloadFile($source, $destination)
-    } catch {
-      write-error $_.Exception
-      $LASTEXITCODE = 1
-    }
-}
-
-function Add-Paths() {
-  foreach ($path in $paths) {
-    Add-Path -Path $path
-  }
-}
-
-function Add-Path($Path) {
-  [System.Collections.ArrayList]$EnvPaths = $env:Path -split ";"
-  if($EnvPaths -notcontains $Path) {
-    $env:Path += ";$Path"
-    [Environment]::SetEnvironmentVariable("Path", $env:Path,[System.EnvironmentVariableTarget]::User)
-    Write-Host "Added $path to PATH."
-  }
-}
-
-function Trap-Status($item) {
-  if($LASTEXITCODE -eq 0) {
-    $installed_successfully.add($item)
-  } else {
-    $install_failed.add($item)
-  }
-}
-
-function Show-Report() {
-  Write-Host "`n########## Vela Configuration Report##########"
-  if($installed_successfully.count -gt 0) {
-    Write-Host "Successfully installed:`n"($installed_successfully -join "`n")
-  }
-  if($install_failed -gt 0) {
-    Write-Host "Installs which failed:`n"($install_failed -join "`n")
-  }
 }
 
 Configure-Vela-Workspace
---
+++ modules/Vela-Utils.psm1
@@ -0,0 +1,110 @@
+[System.Collections.ArrayList]$installed_successfully = @( )
+[System.Collections.ArrayList]$install_failed = @( )
+
+function Install-Apps($Apps) {
+  Install-Chocolatey
+  foreach ($app in $Apps) {
+    choco install $app -y
+    Push-Status -item $app
+  }
+}
+
+function Install-Chocolatey() {
+  if(-Not (Get-Command choco -errorAction SilentlyContinue)) {
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+  }
+}
+
+function Install-WinFeatures($Features) {
+  foreach ($feature in $windows_features) {
+    Install-WindowsFeature $feature
+    Push-Status -item $feature
+  }
+}
+
+function Install-PipPackages($Packages) {
+  Install-Python
+  foreach ($package in $Packages) {
+    pip3 install $package
+    Push-Status -item "Pip - $package"
+  }
+}
+
+function Install-Python() {
+  if(-Not (Get-Command pip3 -ErrorAction SilentlyContinue)) {
+    Install-Chocolatey
+    choco install python -y
+  }
+}
+
+function Install-CygwinPackages ($Packages) {
+  Install-Cygwin
+  foreach ($package in $Packages) {
+    cmd /c C:\tools\cygwin\bin\bash.exe --login -c "/usr/bin/apt-cyg install $package"
+    Push-Status -item "Cygwin - $package"
+  }
+}
+
+function Install-Cygwin() {
+  if (-Not (Test-Path C:\tools\cygwin\bin\bash.exe)) {
+    choco install cygwin -y
+  }
+  if (-Not (Test-Path C:\tools\cygwin\bin\apt-cyg)) {
+    Get-RemoteFile("https://raw.githubusercontent.com/transcode-open/apt-cyg/master/apt-cyg",
+                   "C:\tools\cygwin\bin\apt-cyg")
+  }
+}
+
+function Get-RemoteFiles($Remote_Files) {
+  foreach ($remote_file in $Remote_Files.GetEnumerator()) {
+    if (-Not (Test-Path $remote_file.Value)) {
+      Get-RemoteFile -Source $remote_file.Name.toString() -Destination $remote_file.Value
+    } else {
+      Write-Host $remote_file.Name.toString() " is already installed."
+    }
+    Push-Status -item $remote_file.Name
+  }
+}
+
+function Get-RemoteFile($Source, $Destination) {
+  Write-Host "Downloading $source to $destination"
+    try {
+      (New-Object System.Net.WebClient).DownloadFile($source, $destination)
+    } catch {
+      write-error $_.Exception
+      $LASTEXITCODE = 1
+    }
+}
+
+function Add-Paths($Paths) {
+  foreach ($path in $Paths) {
+    Add-Path -Path $path
+  }
+}
+
+function Add-Path($Path) {
+  [System.Collections.ArrayList]$EnvPaths = $env:Path -split ";"
+  if($EnvPaths -notcontains $Path) {
+    $env:Path += ";$Path"
+    [Environment]::SetEnvironmentVariable("Path", $env:Path,[System.EnvironmentVariableTarget]::User)
+    Write-Host "Added $path to PATH."
+  }
+}
+
+function Push-Status($item) {
+  if($LASTEXITCODE -eq 0) {
+    $installed_successfully.add($item)
+  } else {
+    $install_failed.add($item)
+  }
+}
+
+function Show-Report() {
+  Write-Host "`n########## Vela Configuration Report##########"
+  if($installed_successfully.count -gt 0) {
+    Write-Host "Successfully installed:`n"($installed_successfully -join "`n")
+  }
+  if($install_failed -gt 0) {
+    Write-Error "Installs which failed:`n"($install_failed -join "`n")
+  }
+}
